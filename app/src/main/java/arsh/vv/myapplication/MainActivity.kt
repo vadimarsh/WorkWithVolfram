3072a4936df69af7aaaaf190030e0c63ce9c69e3
vadimarsh <59313785+vadimarsh@users.noreply.github.com>
2021-02-27 20:01:31 +0800

    added voice recognizing and text-to-voice functionality

--- app/src/main/java/arsh/vv/myapplication/MainActivity.kt
+++ app/src/main/java/arsh/vv/myapplication/MainActivity.kt
@@ -1,20 +1,29 @@
 package arsh.vv.myapplication
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wolfram.alpha.WAEngine
 import com.wolfram.alpha.WAPlainText
+import java.util.*
+import android.speech.tts.TextToSpeech
 
 class MainActivity : AppCompatActivity() {
-    lateinit var questionInput: TextView
-    lateinit var answerText: TextView
-
-    lateinit var searchButton: Button
-    private val wolframAppID = "xxxxxx-xxxxxxxxxxx"
+    private lateinit var questionInput: TextView
+    private lateinit var answerText: TextView
+    private lateinit var searchButton: Button
+    private lateinit var speakButton: Button
+    private val wolframAppID = ""
     private val engine = WAEngine()
+    private lateinit var readAnswer: FloatingActionButton
+    lateinit var textToSpeech : TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +33,55 @@ class MainActivity : AppCompatActivity() {
         searchButton = findViewById(R.id.search_button)
         answerText = findViewById(R.id.text_output)
         searchButton.setOnClickListener { askWolfram(questionInput.text.toString()) }
+        speakButton = findViewById(R.id.speak_button)
+        readAnswer = findViewById(R.id.read_answer)
         engine.appID = wolframAppID
         engine.addFormat("plaintext")
+        textToSpeech = TextToSpeech(this, TextToSpeech.OnInitListener {})
+
+        val intentRecognizing = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What do you want to know?")
+        speakButton.setOnClickListener {
+            try {
+                startActivityForResult(intentRecognizing,1)
+            }
+            catch (a: ActivityNotFoundException) {
+                Toast.makeText(
+                    applicationContext,
+                    "Sorry your device not supported",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+        textToSpeech.language = Locale.US
+
+
+        readAnswer.setOnClickListener {
+            textToSpeech.stop()
+
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK && data !=null){
+                val result: ArrayList<String>? = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                val questionText: String? = result?.get(0)
+                if (questionText != null){
+                    questionInput.text = questionText
+                }
+            }
+        }
+
+    }
     fun askWolfram(question: String) {
 
         val query = engine.createQuery()
@@ -36,6 +90,7 @@ class MainActivity : AppCompatActivity() {
         Thread(Runnable {
             val queryResult = engine.performQuery(query)
             answerText.post {
+                var speechRequest = 0
                 if (queryResult.isError) {
                     answerText.text = queryResult.errorMessage
                     Log.d("volfram", queryResult.errorMessage)
@@ -50,6 +105,9 @@ class MainActivity : AppCompatActivity() {
                                     if (element is WAPlainText) {
                                         Log.d("volfram", element.text)
                                         answerText.text = answerText.text.toString() + "\n"+element.text
+
+                                        textToSpeech.speak(element.text, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
+                                        speechRequest += 1
                                     }
                                 }
                             }
@@ -58,6 +116,7 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                 }
+
             }
         }).start()
     }
